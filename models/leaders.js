89198f55bf8183d4d50c90f7083a00a3c99745cd
Alexei Popkov <reystleen@gmail.com>
2018-03-11 14:48:06 +0100

    Assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+
+var Leaders = mongoose.model('leaders', leaderSchema);
+
+module.exports = Leaders;
