706a3b35cd5ddd390c2102ff7af5bd0df079af76
Alexei Popkov <reystleen@gmail.com>
2018-03-17 14:46:04 +0100

    Assignment 3 | Task 1

--- authenticate.js
+++ authenticate.js
@@ -38,7 +38,7 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
-exports.verifyAdminUser = (req, res, next) => {
+exports.verifyAdmin = (req, res, next) => {
     if (req.user.admin) {
         next();
     } else {
